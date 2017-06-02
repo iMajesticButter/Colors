9680c0993bc6a2512665a710d9a33bff5a4357ab
iMajesticButter <bl.wolenetz@gmail.com>
2017-06-02 16:03:47 -0700

    Added levels and fixed bugs

---
+++ Project/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelHandler : MonoBehaviour {
+	public GameObject colorManager = null;
+	public int MaxLevels = 50;
+	public int Level = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Level >= MaxLevels) {
+			Debug.Log ("win");
+			Application.Quit ();
+		}
+		gameObject.GetComponent<Text> ().text = "Level: "+Level+"/"+MaxLevels;
+	}
+}
